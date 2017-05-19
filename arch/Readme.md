919a278ff37b7a424e76096b2e1d5299404b838c
Roman Heinrich <roman.heinrich@gmail.com>
2017-05-19 11:33:50 +0200

    some arch linux files...

---
+++ arch/Readme.md
@@ -0,0 +1,7 @@
+Images to take as base:
+  - https://hub.docker.com/r/pritunl/archlinux/
+  - https://hub.docker.com/r/finalduty/archlinux/
+
+
+
+  - https://hub.docker.com/r/oddlid/arch-desktop/
---
+++ arch/src/setpass.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+prog=/usr/sbin/vncpasswd
+mypass="vncrocks"
+
+/usr/bin/expect <<EOF
+spawn "$prog"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect "Would you like to enter a view-only password (y/n)?"
+send "n\r"
+expect eof
+exit
+EOF
